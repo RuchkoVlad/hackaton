20893050d9818f497a707d4c9a4c3031a0f61142
Vlad Ruchko <necessarydp@gmail.com>
2019-10-07 20:35:07 +0300

    as

---
+++ build/precache-manifest.c8246acf327f00ba5eafeb87c5b03bc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe2b1028264daab2633766acec46eed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "175a95ac604ac033fe22",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "76c00453e496b98cd094",
+    "url": "/static/css/main.8f6f6f83.chunk.css"
+  },
+  {
+    "revision": "175a95ac604ac033fe22",
+    "url": "/static/js/2.d7362508.chunk.js"
+  },
+  {
+    "revision": "76c00453e496b98cd094",
+    "url": "/static/js/main.1bb45298.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
